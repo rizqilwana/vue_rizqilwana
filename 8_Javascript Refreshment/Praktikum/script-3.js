46e6fbf8a95e9d8b82eab280f202eee32e7b77ef
Rizqil Wana <70480715+rizqilwana@users.noreply.github.com>
2022-03-07 17:40:55 +0700

    update task

---
+++ 8_Javascript Refreshment/Praktikum/script-3.js
@@ -0,0 +1,10 @@
+//soal no 5
+
+var value = [1,2,3,4,5,6];
+var kali2 = value.map(function(e)
+)
+{
+    return e * 2 ;
+});
+    console.log(angka.join(,));
+
---
+++ 8_Javascript Refreshment/Praktikum/script-4.js
@@ -0,0 +1,8 @@
+// soal no 4
+var bdays = ['10-17','05-19','20-19']);
+var bdays2 = bdays.map(function(e)
+)
+{
+    return e ;
+});
+    console.log(angka.join(/));
